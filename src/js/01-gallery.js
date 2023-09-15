af0b250a3a00abb7bc66b43856c1abe84343deeb
rodenkoslava <131722989+rodenkoslava@users.noreply.github.com>
2023-09-15 13:08:58 +0200

    last commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,7 @@
 // Add imports above this line
+// Описан в документации
 import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
